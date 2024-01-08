82e165f82fc3d3ea6837a07572864f5de5e6c9ad
Artem <silovik1989@gmail.com>
2024-01-08 19:46:55 +0200

    log+pass age+name

---
+++ HW/lesson2/hw.js
@@ -0,0 +1,5 @@
+const login = "Taras28";
+const password = 123456;
+let name = "Taras";
+let age = 28;
+console.log(`Моє ім'я ${name} і мені ${age} років\n ${login}\n ${password}`);
---
+++ Lessons/Lesson2/index.js
@@ -0,0 +1,16 @@
+let a = 11; //number
+let b = 31;
+let c = a + b;
+let name = "ivan"; //string
+// console.log("сума a і b\n\n\n\n\n", a + b);
+// console.log(a + c);
+// console.log(c + b);
+// console.log("його звати", name, "має років", c);
+// console.log(`його звати ${name} має років ${c}`);
+let rain = true; //boolean
+console.log(rain);
+console.log(a == b);
+console.log(a != b);
+let d = 1;
+let e = "1";
+console.log(d === e);
