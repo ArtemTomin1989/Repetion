379ecab15158fd0f5b097da4c01c68e5a00f9e6a
Artem <silovik1989@gmail.com>
2024-01-26 15:52:28 +0200

    doin hw2 lesson 6

---
+++ HW/lesson6/hw2.js
@@ -0,0 +1,11 @@
+let a;
+
+if (a < 13) {
+  console.log("дитина");
+} else if (a > 13 && a <= 17) {
+  console.log("підліток");
+} else if (a > 18 && a <= 59) {
+  console.log("дорослий");
+} else if (a >= 60) {
+  console.log("літня людина");
+}
