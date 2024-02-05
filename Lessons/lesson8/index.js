146b33c270e5d7f3c4904f30135b2399f8558704
Artem <silovik1989@gmail.com>
2024-02-05 20:40:20 +0200

    hw8

---
+++ Lessons/lesson8/index.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 10; i++) {
+  //i - кількість разів спрацювання циклу
+  console.log("0");
+}
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 20; i++) {
+  console.log(i ** 2);
+}
+let star = "*";
+for (let i = 0; i < 5; i++) {
+  console.log(star);
+  star = star + "*";
+}
