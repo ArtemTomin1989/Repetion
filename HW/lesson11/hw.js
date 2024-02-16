cc4a52585983d8f4a9fd014f556141295b3f18c6
Artem <silovik1989@gmail.com>
2024-02-16 17:09:24 +0200

    learn function

---
+++ HW/lesson11/hw.js
@@ -0,0 +1,13 @@
+function operacii(a, b) {
+  let suma = a + b;
+  let vidnimannya = a - b;
+  let mnozhennya = a * b;
+  let dilennya = a / b;
+
+  console.log(`Сума ${a} і ${b} дорівнює ${suma}`);
+  console.log(`Різниця ${a} і ${b} дорівнює ${vidnimannya}`);
+  console.log(`Добуток ${a} і ${b} дорівнює ${mnozhennya}`);
+  console.log(`Частка ${a} і ${b} дорівнює ${dilennya}`);
+}
+
+operacii(10, 5);
--- Lessons/lesson11/index.js
+++ Lessons/lesson11/index.js
@@ -1,21 +1,27 @@
 let a = 3;
-for (var i = 0; i <= a; i++) {
+for (let i = 0; i <= a; i++) {
   console.log(i);
 }
 let b = 4;
-for (var i = 0; i <= b; i++) {
+for (let i = 0; i <= b; i++) {
   console.log(i);
 }
 let c = 5;
-for (var i = 0; i <= c; i++) {
+for (let i = 0; i <= c; i++) {
   console.log(i);
 }
 console.log("ahahahaahahahaahaahaahaahaahaahaahaahaahaaha");
 function number(num) {
-  for (var i = 0; i <= num; i++) {
+  for (let i = 0; i <= num; i++) {
     console.log(i);
   }
 }
 number(3);
 number(4);
 number(5);
+
+function HW() {
+  console.log("Hello World");
+}
+HW();
+
