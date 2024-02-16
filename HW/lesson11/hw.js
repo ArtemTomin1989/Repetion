d9eb001c1fdd7e66666bb03b87ebbe195d3c4944
Artem <silovik1989@gmail.com>
2024-02-16 17:33:41 +0200

    ideal №2

--- HW/lesson11/hw.js
+++ HW/lesson11/hw.js
@@ -1,20 +1,20 @@
 function dodavannya(a, b) {
-  console.log(`Сума = ${a + b}`);
+  console.log(`Сума чисел ${a} і ${b} = ${a + b}`);
 }
 dodavannya(10, 5);
 
 function vidnimannya(a, b) {
-  console.log(`Різниця = ${a - b}`);
+  console.log(`Різниця чисел ${a} і ${b} = ${a - b}`);
 }
 
 vidnimannya(10, 5);
 
 function mnozhennya(a, b) {
-  console.log(`Добуток = ${a * b}`);
+  console.log(`Добуток чисел ${a} і ${b} = ${a * b}`);
 }
 mnozhennya(10, 5);
 
 function dilennya(a, b) {
-  console.log(`Частка = ${a / b}`);
+  console.log(`Частка чисел ${a} і ${b} = ${a / b}`);
 }
 dilennya(10, 5);
