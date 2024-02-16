90525969d5d5fbdd2f48e78bf6e7715504ab6d93
Artem <silovik1989@gmail.com>
2024-02-16 17:15:15 +0200

    repair

--- HW/lesson11/hw.js
+++ HW/lesson11/hw.js
@@ -1,13 +1,23 @@
-function operacii(a, b) {
+function dodavannya(a, b) {
   let suma = a + b;
-  let vidnimannya = a - b;
-  let mnozhennya = a * b;
-  let dilennya = a / b;
-
   console.log(`Сума ${a} і ${b} дорівнює ${suma}`);
+}
+dodavannya(10, 5);
+
+function vidnimannya(a, b) {
+  let vidnimannya = a - b;
   console.log(`Різниця ${a} і ${b} дорівнює ${vidnimannya}`);
+}
+vidnimannya(10, 5);
+
+function mnozhennya(a, b) {
+  let mnozhennya = a * b;
   console.log(`Добуток ${a} і ${b} дорівнює ${mnozhennya}`);
+}
+mnozhennya(10, 5);
+
+function dilennya(a, b) {
+  let dilennya = a / b;
   console.log(`Частка ${a} і ${b} дорівнює ${dilennya}`);
 }
-
-operacii(10, 5);
+dilennya(10, 5);
