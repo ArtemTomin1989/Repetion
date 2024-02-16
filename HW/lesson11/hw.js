9b3332512bee67994c0b9e2365bef83e3f2297ce
Artem <silovik1989@gmail.com>
2024-02-16 17:22:32 +0200

    ideal

--- HW/lesson11/hw.js
+++ HW/lesson11/hw.js
@@ -1,23 +1,20 @@
 function dodavannya(a, b) {
-  let suma = a + b;
-  console.log(`Сума ${a} і ${b} дорівнює ${suma}`);
+  console.log(`Сума = ${a + b}`);
 }
 dodavannya(10, 5);
 
 function vidnimannya(a, b) {
-  let vidnimannya = a - b;
-  console.log(`Різниця ${a} і ${b} дорівнює ${vidnimannya}`);
+  console.log(`Різниця = ${a - b}`);
 }
+
 vidnimannya(10, 5);
 
 function mnozhennya(a, b) {
-  let mnozhennya = a * b;
-  console.log(`Добуток ${a} і ${b} дорівнює ${mnozhennya}`);
+  console.log(`Добуток = ${a * b}`);
 }
 mnozhennya(10, 5);
 
 function dilennya(a, b) {
-  let dilennya = a / b;
-  console.log(`Частка ${a} і ${b} дорівнює ${dilennya}`);
+  console.log(`Частка = ${a / b}`);
 }
 dilennya(10, 5);
